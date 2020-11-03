5d761d557d1e9841a7b1827f114e379dd7f82e32
xiedong <770714526@qq.com>
2020-11-03 18:06:59 +0800

    [JJApp]增加JJAppMain模块

---
+++ JJAppMain/JJAppMain/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  JJAppMain
+//
+//  Created by xiedong on 2020/11/3.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
