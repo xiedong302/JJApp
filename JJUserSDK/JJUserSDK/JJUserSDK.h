44a6b4374faee8804fe563b4fa44a4948334a339
xiedong <770714526@qq.com>
2020-11-20 11:43:08 +0800

    [JJUserSDK]增加userSDK模块

---
+++ JJUserSDK/JJUserSDK/JJUserSDK.h
@@ -0,0 +1,18 @@
+//
+//  JJUserSDK.h
+//  JJUserSDK
+//
+//  Created by xiedong on 2020/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JJUserSDK.
+FOUNDATION_EXPORT double JJUserSDKVersionNumber;
+
+//! Project version string for JJUserSDK.
+FOUNDATION_EXPORT const unsigned char JJUserSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJUserSDK/PublicHeader.h>
+
+
